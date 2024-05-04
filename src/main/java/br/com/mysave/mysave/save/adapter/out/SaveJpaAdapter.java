acd94b3f6b6c48e3ab859df932c89ea206dc3e66
joao pedro <joaopedroalencar@hotmail.com>
2024-05-03 21:35:00 -0300

    feat: add genericMapper em util, add SaveRepository, add SaveJpaAdapter em ./adapter/out

---
+++ src/main/java/br/com/mysave/mysave/save/adapter/out/SaveJpaAdapter.java
@@ -0,0 +1,2 @@
+package br.com.mysave.mysave.save.adapter.out;public class SaveJpaAdapter {
+}
---
+++ src/main/java/br/com/mysave/mysave/save/application/repositories/SaveRepository.java
@@ -0,0 +1,2 @@
+package br.com.mysave.mysave.save.application.repositories;public class SaveRepository {
+}
